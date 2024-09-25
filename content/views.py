c880007588bfdf21f293436f2452b503073739e1
kenneth osborne <kbosborne1982@gmail.com>
2024-09-24 21:51:06 -0400

    first commit

---
+++ content/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def projects_list(request):
+    return render(request, 'content/projects_list.html')
+
+def experience_list(request):
+    return render(request, 'content/experience_list.html')
