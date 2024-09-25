9c428019105a4dbbc18dfa44ba49168aa944d139
kenneth osborne <kbosborne1982@gmail.com>
2024-09-24 22:47:10 -0400

    class 3 upload

--- content/models.py
+++ content/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Skill(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 
@@ -13,3 +16,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='static/img')
     repository = models.URLField()
     skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return f"{self.name} - ({self.year})"
