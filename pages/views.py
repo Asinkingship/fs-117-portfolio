2fed08fd1e8a7010b22bfd5f8d28d22eabe19b48
kenneth osborne <kbosborne1982@gmail.com>
2024-10-01 14:24:29 -0400

    added session 4

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -7,3 +9,30 @@ def home_view(request):
 
 def about_view(request):
     return render(request, "pages/about.html")
+
+def contact_view(request):
+    if request.method == 'POST':
+
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            print("Sending")
+
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            email_message = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+            send_mail(
+                "Email from " + name,
+                email_message,
+                email,
+                ["kbosborne1982@gmail.com"]
+            )
+
+        else:
+            print("Form not valid")
+    else:
+        form = ContactForm()
+    return render(request, "pages/contact.html", {'form': form})
